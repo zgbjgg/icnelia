e707417e2c3378d03424e91cded78f23ece54710
Jorge Garrido <jorge.garrido@morelosoft.com>
2012-10-09 16:45:46 -0500

    initial

---
+++ include/icnelia.hrl
@@ -0,0 +1,41 @@
+%
+% ICNELIA : Helper for compile, clean and create run scripts for 
+%           an erlang OTP application
+%
+% Copyright (C) 2012, Jorge Garrido <jorge.garrido@morelosoft.com>
+%
+% All rights reserved.
+%
+
+% include dir
+-define(include_dir, "include/").
+
+% source dir
+-define(source_dir, "src/").
+
+% ebin (compiled) dir
+-define(ebin_dir, "ebin/").
+
+% wildcard for .erl on source dir
+-define(src_erl, "src/*.erl").
+
+% wildcard for .beam on ebin dir
+-define(ebin_beam, "ebin/*.beam").
+
+% wildcard for .app on ebin dir
+-define(src_app_ebin, "ebin/*.app").
+
+% wildcard for .app.src on src dir
+-define(src_app_src, "src/*.app.src").
+
+% message when compiling
+-define(msg_c, "compiling ~s ~n").
+
+% message when cleaning
+-define(msg_d, "cleaning ~s ~n").
+
+% config file (must be called 'icnelia.config').
+-define(cfg_file, "icnelia.config").
+
+% name for runner script
+-define(run, "run").
