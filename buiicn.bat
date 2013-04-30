6bb2423a91b8b76d7f1125de3a529f2e28c7c9f8
Jorge Garrido <jorge.garrido@morelosoft.com>
2013-04-30 10:17:57 -0500

    comments on .bat file

--- buiicn.bat
+++ buiicn.bat
@@ -1,8 +1,12 @@
 REM ICNELIA : Helper for compile, clean and create run scripts for
 REM         an erlang OTP application
 REM
-REM Copyright (C) 2012, Jorge Garrido <jorge.garrido@morelosoft.com>
-REM All rights reserved.
+REM Copyright (C) 2012, Jorge Garrido <zgbjgg@gmail.com> All rights reserved.
+REM 
+REM This Source Code Form is subject to the terms of the Mozilla Public
+REM License, v. 2.0. If a copy of the MPL was not distributed with this
+REM file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 
 @ECHO building on win32...
 escript.exe buiicn
