0fad6bee27716a8007fd80508a7e1d302ae69792
Jorge Garrido <jorge.garrido@morelosoft.com>
2013-04-30 10:24:28 -0500

    avoid script with run_erl on win32

--- src/icnelia_cmds.erl
+++ src/icnelia_cmds.erl
@@ -2,10 +2,13 @@
 % ICNELIA : Helper for compile, clean and create run scripts for 
 %           an erlang OTP application
 %
-% Copyright (C) 2012, Jorge Garrido <jorge.garrido@morelosoft.com>
+% Copyright (C) 2012-2013, Jorge Garrido <zgbjgg@gmail.com> All rights reserved.
 %
-% All rights reserved.
+% This Source Code Form is subject to the terms of the Mozilla Public
+% License, v. 2.0. If a copy of the MPL was not distributed with this
+% file, You can obtain one at http://mozilla.org/MPL/2.0/.
 %
+
 -module(icnelia_cmds).
 
 -export([cmd/1]).
