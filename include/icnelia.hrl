5fc86040f8b62e853a0971e94f444242f74ab9b2
Jorge Garrido <jorge.garrido@morelosoft.com>
2013-04-30 10:19:23 -0500

    escape backslash on erl command, append comments as REM

--- include/icnelia.hrl
+++ include/icnelia.hrl
@@ -2,9 +2,11 @@
 % ICNELIA : Helper for compile, clean and create run scripts for 
 %           an erlang OTP application
 %
-% Copyright (C) 2012, Jorge Garrido <jorge.garrido@morelosoft.com>
+% Copyright (C) 2012-2013, Jorge Garrido <zgbjgg@gmail.com> All rights reserved.
 %
-% All rights reserved.
+% This Source Code Form is subject to the terms of the Mozilla Public
+% License, v. 2.0. If a copy of the MPL was not distributed with this
+% file, You can obtain one at http://mozilla.org/MPL/2.0/.
 %
 
 % include dir
