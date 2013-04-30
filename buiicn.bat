6cb5a4709bcd83ffbf1c7672da9e5a39f87062ea
Jorge Garrido <jorge.garrido@morelosoft.com>
2013-04-30 10:01:24 -0500

    comments on bat script

--- buiicn.bat
+++ buiicn.bat
@@ -1,2 +1,8 @@
+REM ICNELIA : Helper for compile, clean and create run scripts for
+REM         an erlang OTP application
+REM
+REM Copyright (C) 2012, Jorge Garrido <jorge.garrido@morelosoft.com>
+REM All rights reserved.
+
 @ECHO building on win32...
 escript.exe buiicn
